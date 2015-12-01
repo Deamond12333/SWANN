679520589f4013c4a6a9f572f802f1d45f9a75e7
Deamond12333 <andrei-587@mail.ru>
2015-12-01 19:02:22 +0600

    5%

--- SWANN/SWANN/Program.cs
+++ SWANN/SWANN/Program.cs
@@ -16,7 +16,7 @@ namespace SWANN
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Home());
         }
     }
 }
---
+++ SWANN/SWANN/SWANN.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SWANN
+{
+    class SWANN
+    {
+        public SWANN(string function, double x0, double t)
+        {
+            int k = 0;
+        }
+
+        private double parseFunction(string function, double x)
+        {
+            double result = 0;
+            for (int i = 0; i < function.Length; ++i)
+            {
+                if (function.Substring(i, 1) == "^")
+                {
+                    int begin = i + 1;
+                    int end = 0;
+                    for (int j = begin; j < function.Length; ++j)
+                    {
+                        if (function.Substring(i, 1) == " ")
+                            result += Math.Pow(x, double.Parse(function.Substring(begin, end - 1)));
+                        else end++;
+                    }
+                }
+            }
+            return result;
+        }
+    }
+}
